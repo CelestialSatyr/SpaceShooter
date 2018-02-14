05576cae63a96eab4e37e7bebdc422f033002df0
Luc <CelestialSatyr@gmail.com>
2018-02-14 12:26:48 +0100

    Heb de Mines en ItemRandomizer toegevoegd.

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public float ItemRandomizer;
+
+    public void Start()
+    {
+        ItemRandomizer = Random.Range(0, 5);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (ItemRandomizer == 0)
+        {
+            //Effect 1 (Positive)
+        }
+        if (ItemRandomizer == 1)
+        {
+            //Effect 2 (Negative)
+        }
+        if (ItemRandomizer == 2)
+        {
+            //Effect 3 (Positive)
+        }
+        if (ItemRandomizer == 3)
+        {
+            //Effect 4 (Negative)
+        }
+        if (ItemRandomizer == 4)
+        {
+            //Effect 5 (Positive)
+        }
+        if (ItemRandomizer == 5)
+        {
+            //Effect 6 (Negative)
+        }
+    }
+}
---
+++ Assets/Scripts/Mines.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mines : MonoBehaviour
+{
+    [SerializeField] private MeshCollider OuterRange;
+    [SerializeField] private List<GameObject> Enemies;
+    [SerializeField] private MeshCollider Touching;
+
+    void Start()
+    {
+        if (OuterRange == null) { OuterRange = GetComponent<MeshCollider>(); }
+        if (Touching == null) { Touching = GetComponent<MeshCollider>(); }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        foreach(GameObject Enemy in Enemies)
+        {
+            Destroy(Enemy);
+            Enemies.Remove(transform.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Enemies.Add(other.transform.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Enemies.Remove(other.transform.gameObject);
+        }
+    }
+}
